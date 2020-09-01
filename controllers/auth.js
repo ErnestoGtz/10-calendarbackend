28d1c3335cfbce0a405b5ccd30ceb12c7ac2f675
Ernesto Emmanuel Gutierrez Muñoz <ernesto.gtz@itcoach.com.mx>
2020-09-01 09:12:08 -0500

    continue

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,22 +3,37 @@ const {response} = require('express');
 
 const newUser = (req,res = response) =>{
     
+    const {name,email,password} = req.body;
+    
+    if(name.length < 5){
+        return res.status(400).json({
+            ok:true,
+            msg:'El nombre debe de ser de 5 letras',
+        });
+    }
+    
     res.json({
         ok:true,
         msg:'registo',
-        user:req.body
-    })
+        name,
+        email,
+        password
+    });
 }
 
-const login = (req,res)=>{
-        res.json({
+const login = (req,res = response)=>{
+    
+    const {email,password} = req.body;
+    
+    res.json({
             ok:true,
-            msg:'login'
+            msg:'login',
+            email,password
         });
     
 }
 
-const renew = (req,res)=>{
+const renew = (req,res = response)=>{
     res.json({
         ok:true,
         msg:'renew'
